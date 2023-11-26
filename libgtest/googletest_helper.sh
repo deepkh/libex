92fb0223af22663c8429eda9f3af4b44d7b6d55a
deepkh <deepkh@gmail.com>
2023-11-26 14:47:13 +0800

    googletest build fail by x86_64-w64-mingw32

--- libgtest/googletest_helper.sh
+++ libgtest/googletest_helper.sh
@@ -26,6 +26,28 @@ build_target_and_install_linux64() {
 	popd
 }
 
+###
+### Build fail by x86_64-w64-mingw32 due to 
+### Googletest does not compile with POSIX Threads for Windows #3577
+### https://github.com/google/googletest/issues/3577
+### 
+### BTW, This problem seems to exist for many years. 
+### 
+build_target_and_install_win64() {
+	echo "==== $0 build_target_and_install_win64 $1 ===="
+	pushd ${1}
+
+	rm -rf build
+	mkdir build
+	pushd build
+	cmake -DCMAKE_INSTALL_PREFIX=${RUNTIME} -DCMAKE_TOOLCHAIN_FILE=../mingw-w64-x86_64.cmake -DGTEST_HAS_PTHREAD=1 ../
+	make -j4
+	make install
+	popd
+	
+	popd
+}
+
 build_target_and_install() {
 	if [[ "${HOST}" = "Linux"  && "${TARGET}" == "linux64" ]];then
 		build_target_and_install_linux64 ${1}
